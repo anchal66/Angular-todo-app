c14695e54aa2671dc0a8b0a3ab4b13060da07f4f
Avinash Anchal <you@example.comanchalavinash66@gmail.com>
2019-06-28 09:11:57 +0530

    error resolved for removing from app.module

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,14 +3,12 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-import { WelccomeComponent } from './welccome/welccome.component';
 import { WelcomeComponent } from './welcome/welcome.component';
 import { LoginComponent } from './login/login.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    WelccomeComponent,
     WelcomeComponent,
     LoginComponent
   ],

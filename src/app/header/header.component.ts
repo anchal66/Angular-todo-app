b6976f164f048ffebe9f16397c75402b1a655cbd
Avinash Anchal <you@example.comanchalavinash66@gmail.com>
2019-06-30 11:40:58 +0530

    shwing links according to authitication

--- src/app/header/header.component.ts
+++ src/app/header/header.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { HardCodedAuthenticationService } from '../service/hard-coded-authentication.service';
 
 @Component({
   selector: 'app-header',
@@ -7,9 +8,11 @@ import { Component, OnInit } from '@angular/core';
 })
 export class HeaderComponent implements OnInit {
 
-  constructor() { }
+  // isUserLoggedIn=false
+  constructor(private hardCodedAuthenticationService:HardCodedAuthenticationService) { }
 
   ngOnInit() {
+    // this.isUserLoggedIn=this.hardCodedAuthenticationService.isUserLoggedIn()
   }
 
 }

97a3144a6407422a3f9a5e42be23efa489044c9f
Avinash Anchal <you@example.comanchalavinash66@gmail.com>
2019-06-30 10:30:41 +0530

    Added class for todo and header footer bootstrap

--- src/app/list-todos/list-todos.component.ts
+++ src/app/list-todos/list-todos.component.ts
@@ -1,16 +1,29 @@
 import { Component, OnInit } from '@angular/core';
 
+export class Todo{
+  constructor(
+    public id:number,
+    public description:string,
+    public done:boolean,
+    public targetDate:Date
+  ){}
+}
 @Component({
   selector: 'app-list-todos',
   templateUrl: './list-todos.component.html',
   styleUrls: ['./list-todos.component.css']
 })
+
 export class ListTodosComponent implements OnInit {
 
   todos=[
-    {id:1, desciption: 'Learn something'},
-    {id:2, desciption: 'Learn something2'},
-    {id:3, desciption: 'Learn something3'}
+    new Todo(1,"Learn Dance", false, new Date()),
+    new Todo(2,"Learn Sing", true, new Date()),
+    new Todo(3,"Learn Jump", false, new Date()),
+    new Todo(4,"Learn Code", true, new Date())
+    // {id:1, desciption: 'Learn something'},
+    // {id:2, desciption: 'Learn something2'},
+    // {id:3, desciption: 'Learn something3'}
   ]
   constructor() { }
 

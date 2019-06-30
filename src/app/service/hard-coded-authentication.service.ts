afc66f3f6786bb434ffd010343856095f9ee9f95
Avinash Anchal <you@example.comanchalavinash66@gmail.com>
2019-06-30 11:27:46 +0530

    Created service for session storage4authentication

--- src/app/service/hard-coded-authentication.service.ts
+++ src/app/service/hard-coded-authentication.service.ts
@@ -8,8 +8,13 @@ export class HardCodedAuthenticationService {
   constructor() { }
   authenticate(username, password){
     if(username==='anchal' && password==='anchal'){
+      sessionStorage.setItem('authenticatedUser', username)
       return true;
     }
     return false; 
+  }
+  isUserLoggedIn(){
+    let user=sessionStorage.getItem('authenticatedUser')
+    return !(user===null)
   }
 }
